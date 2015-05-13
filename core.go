4784fda133aaf4fb64451fb1d88dd24f2d7f495b
ssmekel <shaun.smekel@theorem.net.au>
2015-05-14 09:09:08 +1000

    Added command for checking the existence of a file/dir
    Signed-off-by: ssmekel <shaun.smekel@theorem.net.au>

--- core.go
+++ core.go
@@ -1,31 +1,40 @@
 package main
 
 import (
-
-  "os/exec"
-  "sync"
-  "fmt"
-  "strings"
-
+	"fmt"
+	"os/exec"
+	"strings"
+	"sync"
 )
 
 func ExecCmd(cmd string, wg *sync.WaitGroup) {
 
-  fmt.Println("command is ",cmd)
+	fmt.Println("command is ", cmd)
 
-  // splitting head => g++ parts => rest of the command
-  parts := strings.Fields(cmd)
-  head := parts[0]
-  parts = parts[1:len(parts)]
+	// splitting head => g++ parts => rest of the command
+	parts := strings.Fields(cmd)
+	head := parts[0]
+	parts = parts[1:len(parts)]
 
-  out, err := exec.Command(head,parts...).Output()
-  
-  if err != nil {
-    fmt.Printf("%s", err)
-  }
-  
-  fmt.Printf("%s", out)
-  
-  wg.Done() // Need to signal to waitgroup that this goroutine is done
-  
+	out, err := exec.Command(head, parts...).Output()
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	fmt.Printf("%s", out)
+
+	wg.Done() // Need to signal to waitgroup that this goroutine is done
+
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
