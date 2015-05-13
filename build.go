2618a77df80738b8315ac45d3160d08695baf049
ssmekel <shaun.smekel@theorem.net.au>
2015-05-14 09:09:22 +1000

    Added code for building templates
    Signed-off-by: ssmekel <shaun.smekel@theorem.net.au>

--- build.go
+++ build.go
@@ -1,19 +1,53 @@
 package main
 
 import (
-
-  "sync"
-
+	"os"
+	"sync"
 )
 
-func Build() {
-  wg := new(sync.WaitGroup)
-  commands := []string{""}
-    
-  for _, str := range commands {
-    wg.Add(1)
-    go ExecCmd(str, wg)
-  }
-  
-  wg.Wait()
+type builder struct {
+	buildDest      string
+	templateSource string
+	envSource      string
 }
+
+func (build *builder) Build(distDest, templateSource, envSource string) {
+
+  // Ensure all required directory entries exist
+  directories := []string{distDest, templateSource, envSource}
+
+  for _, directory := range directories {
+    if _, err exists(directory) && err != nil {
+      fmt.Printf("%s", err)
+    }  
+  }
+
+  // Set variables to point to folders
+  templateFiles, err := ioutil.ReadDir(templateSource)
+  envFiles, err := ioutil.ReadDir(envSource)
+	err := Chdir(distDest)
+
+  // Loop through template files for processing
+  for _, file := range templateFiles {
+
+    // If file is a .yaml send for processing
+    
+    // Add file to dist
+    
+  }
+
+	wg := new(sync.WaitGroup)
+	commands := []string{""}
+
+	for _, str := range commands {
+		wg.Add(1)
+		go ExecCmd(str, wg)
+	}
+
+	wg.Wait()
+}
+
+func (build *builder)  {
+
+}
+
