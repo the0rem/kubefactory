35ba05ba0f2d2b051cc5be3f3b84fddfc16bb3a5
ssmekel <shaun.smekel@theorem.net.au>
2015-05-14 09:06:51 +1000

    Adding foundation for interactive generation of templates
    Signed-off-by: ssmekel <shaun.smekel@theorem.net.au>

--- generate.go
+++ generate.go
@@ -1,19 +1,49 @@
 package main
 
 import (
-
-  "sync"
-
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
 )
 
-func Generate() {
-  wg := new(sync.WaitGroup)
-  commands := []string{""}
-    
-  for _, str := range commands {
-    wg.Add(1)
-    go ExecCmd(str, wg)
-  }
-  
-  wg.Wait()
+type generatorOptions struct {
+	templateSource string
+	envSource      string
+}
+
+func (options *generatorOptions) Generate(templateSource, envSource) {
+
+	consolereader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name : ")
+
+	input, err := consolereader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(input)
+
+	// wg := new(sync.WaitGroup)
+	// commands := []string{""}
+
+	// for _, str := range commands {
+	// 	wg.Add(1)
+	// 	go ExecCmd(str, wg)
+	// }
+
+	// wg.Wait()
+}
+
+func start() {
+
+	consolereader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to the template generator. To get started, answer from the options below:")
+	fmt.Println("[0] Create a new file")
+	fmt.Println("[1] Edit an existing file")
+
+	input, err := consolereader.ReadString('\n')
+
 }
