18d8475656e6fde51d4638cbe61ccc28eed765bb
ssmekel <shaun.smekel@theorem.net.au>
2015-05-14 09:06:03 +1000

    Updated code for creating links
    Signed-off-by: ssmekel <shaun.smekel@theorem.net.au>

--- link.go
+++ link.go
@@ -1,54 +1,60 @@
 package main
 
 import (
-
-  "sync"
-
+	"sync"
 )
 
+/**
+ *
+ */
 type dirSync struct {
-  from, to string
+	from, to string
 }
 
-func (link *dirSync) configure() {
-  
-  link.from = "yomama"
-  link.to = "mymama"
-  
+/**
+ * [func description]
+ * @param  {[type]} link *dirSync)     configure(from, to string [description]
+ * @return {[type]}      [description]
+ */
+func (link *dirSync) configure(from, to string) {
+
+	link.from = from
+	link.to = to
+
 }
 
 func (link *dirSync) List() {
-  wg := new(sync.WaitGroup)
-  commands := []string{""}
-    
-  for _, str := range commands {
-    wg.Add(1)
-    go ExecCmd(str, wg)
-  }
-  
-  wg.Wait()
+	wg := new(sync.WaitGroup)
+	commands := []string{""}
+
+	for _, str := range commands {
+		wg.Add(1)
+		go ExecCmd(str, wg)
+	}
+
+	wg.Wait()
 }
 
 func (link *dirSync) Remove() {
-  wg := new(sync.WaitGroup)
-  commands := []string{""}
-    
-  for _, str := range commands {
-    wg.Add(1)
-    go ExecCmd(str, wg)
-  }
-  
-  wg.Wait()
+	wg := new(sync.WaitGroup)
+	commands := []string{""}
+
+	for _, str := range commands {
+		wg.Add(1)
+		go ExecCmd(str, wg)
+	}
+
+	wg.Wait()
 }
 
 func (link *dirSync) Add() {
-  wg := new(sync.WaitGroup)
-  commands := []string{""}
-    
-  for _, str := range commands {
-    wg.Add(1)
-    go ExecCmd(str, wg)
-  }
-  
-  wg.Wait()
+	wg := new(sync.WaitGroup)
+	commands := []string{""}
+
+	for _, str := range commands {
+		wg.Add(1)
+		go ExecCmd(str, wg)
+	}
+
+	wg.Wait()
 }
